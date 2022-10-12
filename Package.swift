79d5fafb742d119f53e8af3d8c9bf63c68553bd0
Damiaan Dufaux <damiaan.dufaux@theoplayer.com>
2022-10-12 13:54:50 +0200

    Use new connector monorepo structure

--- Package.swift
+++ Package.swift
@@ -4,39 +4,24 @@
 import PackageDescription
 
 let package = Package(
-    name: "THEOplayerConvivaConnector",
+    name: "THEOplayer-Connectors",
     platforms: [ .iOS(.v12) ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(name: "THEOplayerConvivaConnector", targets: ["THEOplayerConvivaConnector"])
+        .library(name: "THEOplayerConnectorConviva", targets: ["THEOplayerConnectorConviva"])
     ],
     dependencies: [
         .package(url: "https://github.com/Conviva/conviva-ios-sdk-spm", from: "4.0.31"),
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.1.4"),
         .package(url: "https://github.com/Dev1an/theoplayer-sdk-ios", branch: "master"), //TODO: replace with official URL
     ],
     targets: [
         .target(
-            name: "THEOplayerConvivaConnector",
+            name: "THEOplayerConnectorConviva",
             dependencies: [
                 .product(name: "THEOplayerSDK", package: "theoplayer-sdk-ios"),
                 .product(name: "ConvivaSDK", package: "conviva-ios-sdk-spm"),
             ],
-            path: "Sources/ConvivaConnector",
-            swiftSettings: [
-                .define("VERIZONMEDIA")
-            ],
-            plugins: [.plugin(name: "TheoFeatureDetectorPlugin")]
-        ),
-        .executableTarget(
-            name: "FeatureMocker",
-            dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser")],
-            resources: [.copy("VerizonMock.txt")]
-        ),
-        .plugin(
-            name: "TheoFeatureDetectorPlugin",
-            capability: .buildTool(),
-            dependencies: ["FeatureMocker"]
+            path: "Code/Conviva/Source"
         ),
     ]
 )
