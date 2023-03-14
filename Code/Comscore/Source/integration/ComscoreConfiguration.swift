e1992a09afa682b9d5d91e1aae5452176e7f93a5
Wonne Joosen <wonne.joosen@theoplayer.com>
2023-03-14 21:14:04 +0100

    Get cusv extraction out part 2

--- Code/Comscore/Source/integration/ComscoreConfiguration.swift
+++ Code/Comscore/Source/integration/ComscoreConfiguration.swift
@@ -1,17 +1,18 @@
 //
-//  theoComscoreConguration.swift
-//  theoplayer-comscore-ios-integration
+//  ComscoreConguration.swift
 //
-//  Copyright © 2021 THEOPlayer. All rights reserved.
+//  Copyright © THEOPlayer. All rights reserved.
 //
 
 import Foundation
+import THEOplayerSDK
 
 public class ComScoreConfiguration {
     public let publisherId: String
     public let applicationName: String
     public var userConsent: ComScoreUserConsent
     public let childDirectedAppMode: Bool
+    public var adIdProcessor: ((THEOplayerSDK.Ad) -> String)? = nil
     public var debug: Bool
 
     // MARK: - initializer
@@ -25,11 +26,12 @@ public class ComScoreConfiguration {
      - enableChildDirectedApplicationMode: Controls collection of advertising id within the app
      - debug: Debug mode
      */
-    public init(publisherId: String, applicationName: String, userConsent: ComScoreUserConsent = .unknown, childDirectedAppMode: Bool = false, debug: Bool = false) {
+    public init(publisherId: String, applicationName: String, userConsent: ComScoreUserConsent = .unknown, childDirectedAppMode: Bool = false, adIdProcessor: ((THEOplayerSDK.Ad) -> String)?,debug: Bool = false) {
         self.publisherId = publisherId
         self.applicationName = applicationName
         self.userConsent = userConsent
         self.childDirectedAppMode = childDirectedAppMode
+        self.adIdProcessor = adIdProcessor
         self.debug = debug
     }
 }
