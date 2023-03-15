7181d09d4caa97b85d8eaee21db9ae94c0fbe398
Wonne Joosen <wonne.joosen@theoplayer.com>
2023-03-15 11:49:49 +0100

    Add example comscore usage

--- Code/Comscore-Examples/Cocoapod/ComscoreReporter/ViewController.swift
+++ Code/Comscore-Examples/Cocoapod/ComscoreReporter/ViewController.swift
@@ -6,14 +6,63 @@
 //
 
 import UIKit
+import THEOplayerSDK
+import THEOplayerConnectorComscore
 
 class ViewController: UIViewController {
+    
+    let comscore = ComscoreConnector(
+        configuration: comscoreConfig,
+        player: THEOplayer(with: nil, configuration: nil),
+        metadata: ComScoreMetadata(mediaType: .shortFormOnDemand, uniqueId: "0123ABC", length: 596, stationTitle: "THEO TV", programTitle: "Big Buck Bunny", episodeTitle: "Example Title", genreName: "animation", classifyAsAudioStream: false)
+    )
+    
+    @IBOutlet weak var playerViewContainer: UIView!
+    var player: THEOplayer { comscore.player }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let playerView = PlayerView(player: player)
+        playerView.translatesAutoresizingMaskIntoConstraints = true
+        playerView.frame = playerViewContainer.bounds
+        playerView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        playerViewContainer.addSubview(playerView)
+        
     }
-
-
+    
+    
+    @IBAction func bbbButtonClicked(_ sender: UIButton) {
+        player.source = SourceDescription(
+            source: TypedSource(
+                src: bigBuckBunnyURL,
+                type: "application/x-mpegurl"
+            ),
+            metadata: MetadataDescription(
+                metadataKeys: nil,
+                title: "Big buck bunny"
+            )
+        )
+        comscore.comscore.update(metadata: ComScoreMetadata(mediaType: .shortFormOnDemand, uniqueId: "0123ABC", length: 596, stationTitle: "THEO TV", programTitle: "Big Buck Bunny", episodeTitle: "Example Title", genreName: "animation", classifyAsAudioStream: false))
+    }
+    
+    @IBAction func starWarsButtonClicked(_ sender: UIButton) {
+        player.source = SourceDescription(
+            source: TypedSource(
+                src: starwarsURL.absoluteString,
+                type: "application/x-mpegurl"
+            ),
+            metadata: MetadataDescription(
+                metadataKeys: nil,
+                title: "Star wars episode VII the force awakens official comic-con 2015 reel (2015)"
+            )
+        )
+        comscore.comscore.update(metadata: ComScoreMetadata(mediaType: .shortFormOnDemand, uniqueId: "4567DEF", length: 211, stationTitle: "THEO TV", programTitle: "Star Wars", episodeTitle: "Episode VII The Force Awakens", genreName: "Science Fiction", classifyAsAudioStream: false))
+    }
 }
+
+let bigBuckBunnyURL = "https://cdn.theoplayer.com/video/big_buck_bunny/big_buck_bunny.m3u8"
+let starwarsURL = URL(string: "https://cdn.theoplayer.com/video/star_wars_episode_vii-the_force_awakens_official_comic-con_2015_reel_(2015)/index-daterange.m3u8")!
 
--- Code/Comscore/Source/ComscoreConnector.swift
+++ Code/Comscore/Source/ComscoreConnector.swift
@@ -6,9 +6,9 @@ public struct ComscoreConnector {
     public let player: THEOplayer
 
 
-    public init(configuration: ComScoreConfiguration, player: THEOplayer) {
+    public init(configuration: ComScoreConfiguration, player: THEOplayer, metadata: ComScoreMetadata) {
         ComScoreAnalytics.start(configuration: configuration)
         self.player = player
-        self.comscore = ComScoreAnalytics.THEOplayerComscoreSDK(player: player, playerVersion: THEOplayer.version, metadata: nil)
+        self.comscore = ComScoreAnalytics.THEOplayerComscoreSDK(player: player, playerVersion: THEOplayer.version, metadata: metadata)
     }
 }
