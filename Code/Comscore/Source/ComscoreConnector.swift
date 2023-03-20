9a14c43d5f0e30436eff0820318ea863d4b4f72c
Wonne Joosen <wonne.joosen@theoplayer.com>
2023-03-20 14:50:58 +0100

    Change naming

--- Code/Comscore/Source/ComscoreConnector.swift
+++ Code/Comscore/Source/ComscoreConnector.swift
@@ -2,13 +2,13 @@ import THEOplayerSDK
 import ComScore
 
 public struct ComscoreConnector {
-    public let comscore: ComScoreStreamingAnalytics
+    public let streamingAnalytics: ComScoreStreamingAnalytics
     public let player: THEOplayer
 
 
     public init(configuration: ComScoreConfiguration, player: THEOplayer, metadata: ComScoreMetadata) {
         ComScoreAnalytics.start(configuration: configuration)
         self.player = player
-        self.comscore = ComScoreAnalytics.THEOplayerComscoreSDK(player: player, playerVersion: THEOplayer.version, metadata: metadata)
+        self.streamingAnalytics = ComScoreAnalytics.THEOplayerComscoreSDK(player: player, playerVersion: THEOplayer.version, metadata: metadata)
     }
 }
