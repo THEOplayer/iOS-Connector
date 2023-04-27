4257d803bda8b4234394262ac1eb0b909afca6af
Damiaan Dufaux <damiaan.dufaux@theoplayer.com>
2023-04-27 15:25:04 +0200

    Do not report empty sessions
    Problem: Calling play and then setting a source, reports two sessions
    Cause: Calling play with no source, creates an empty session. Setting the source closes the previous session and creates a new one for the new source
    Solution: Do not create a new session on source change when transitioning from a nil source to a non-nil source.

--- Code/Conviva/Source/Events/ConvivaReporters/BasicEventConvivaReporter.swift
+++ Code/Conviva/Source/Events/ConvivaReporters/BasicEventConvivaReporter.swift
@@ -10,25 +10,28 @@ import THEOplayerSDK
 
 class BasicEventConvivaReporter: BasicEventProcessor {
     
-    struct Source {
-        var hasNotPlayedSinceSourceChange = true
-        var url: String?
+    struct Session {
+        struct Source {
+            let description: SourceDescription
+            let url: String?
+        }
+        var started = false
+        var source: Source?
     }
     
     /// The endpoint to which all the events are sent
     let conviva: CISVideoAnalytics
     
-    var currentSource = Source()
+    var currentSession = Session()
         
     init(conviva: CISVideoAnalytics) {
         self.conviva = conviva
     }
 
     func play(event: PlayEvent) {
-        guard currentSource.hasNotPlayedSinceSourceChange else { return }
-        currentSource.hasNotPlayedSinceSourceChange = false
-        
+        guard !currentSession.started else { return }
         conviva.reportPlaybackRequested(nil)
+        currentSession.started = true
     }
     
     func playing(event: PlayingEvent) {
@@ -64,25 +67,29 @@ class BasicEventConvivaReporter: BasicEventProcessor {
     }
     
     func sourceChange(event: SourceChangeEvent, selectedSource: String?) {
-        reportEndedIfPlayed()
+        if event.source != currentSession.source?.description, currentSession.source != nil {
+            reportEndedIfPlayed()
+        }
         
-        currentSource = Source()
+        let newSource: Session.Source?
         
-        if let source = selectedSource {
-            currentSource.url = source
+        if let source = event.source, let url = selectedSource {
+            newSource = .init(description: source, url: url)
             let contentInfo = [
                 CIS_SSDK_METADATA_PLAYER_NAME: Utilities.playerFrameworkName,
-                CIS_SSDK_METADATA_STREAM_URL: source,
-                CIS_SSDK_METADATA_ASSET_NAME: event.source?.metadata?.title ?? Utilities.defaultStringValue,
+                CIS_SSDK_METADATA_STREAM_URL: url,
+                CIS_SSDK_METADATA_ASSET_NAME: source.metadata?.title ?? Utilities.defaultStringValue,
                 CIS_SSDK_METADATA_IS_LIVE: NSNumber(value: false),
                 CIS_SSDK_METADATA_DURATION: NSNumber(value: -1)
             ] as [String: Any]
             conviva.setContentInfo(contentInfo)
         } else {
+            newSource = nil
             #if DEBUG
             print("[THEOplayerConnectorConviva] setting unknown source")
             #endif
         }
+        currentSession.source = newSource
     }
     
     func renderedFramerateUpdate(framerate: Float) {
@@ -95,15 +102,14 @@ class BasicEventConvivaReporter: BasicEventProcessor {
     }
     
     func reportEndedIfPlayed() {
-        let hasPlayed = !currentSource.hasNotPlayedSinceSourceChange
-        if hasPlayed {
+        if currentSession.started {
             conviva.reportPlaybackEnded()
-            currentSource = Source()
+            currentSession = Session()
         }
     }
     
     func durationChange(event: DurationChangeEvent) {
-        if let duration = event.duration, currentSource.url != nil {
+        if let duration = event.duration, currentSession.source?.url != nil {
             if duration.isInfinite {
                 conviva.setContentInfo([
                     CIS_SSDK_METADATA_IS_LIVE: NSNumber(value: true)
