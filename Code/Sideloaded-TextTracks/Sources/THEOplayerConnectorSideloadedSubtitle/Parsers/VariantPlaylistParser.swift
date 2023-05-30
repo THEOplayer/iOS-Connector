44540d4b320007816dee2b01af17412b52148171
Daniel Dallos <daniel.dallos@theoplayer.com>
2023-05-30 14:29:06 +0200

    added sideloaded texttracks source code

---
+++ Code/Sideloaded-TextTracks/Sources/THEOplayerConnectorSideloadedSubtitle/Parsers/VariantPlaylistParser.swift
@@ -0,0 +1,68 @@
+//
+//  VariantPlaylistParser.swift
+//  THEOplayer_SDK
+//
+//  Copyright © 2023 THEOplayer. All rights reserved.
+//
+
+import Foundation
+
+class VariantPlaylistParser: PlaylistParser {
+    fileprivate(set) var totalPlayListDuration: Double
+    var constructedManifestArray = [String]()
+    
+    override init(url: URL) {
+        self.totalPlayListDuration = 0
+        super.init(url: url)
+    }
+    
+    func parse(completion: @escaping (_ playlist: VariantPlaylistParser?) -> ()) {
+        self.loadManifest { succ in
+            if succ {
+                self.parseManifest()
+                let constructed = self.constructedManifestArray.joined(separator: "\n")
+                
+                if THEOplayerConnectorSideloadedSubtitle.SHOW_DEBUG_LOGS {
+                    print("[AVSubtitlesLoader] VARIANT: +++++++")
+                    print(constructed)
+                    print("[AVSubtitlesLoader] VARIANT: ------")
+                }
+                
+                if let data = constructed.data(using: .utf8) {
+                    self.manifestData = data
+                }
+                completion(self)
+            } else {
+                completion(nil)
+            }
+        }
+    }
+    
+    fileprivate func parseManifest() {
+        guard let manifestData = self.manifestData, let manifestString = String(data: manifestData, encoding: .utf8) else {
+            #if DEBUG
+            print("[AVSubtitlesLoader] ERROR parsing the variant playlist")
+            #endif
+            return
+        }
+        let allLines = manifestString.components(separatedBy: "\n")
+        var iterator = allLines.makeIterator()
+        
+        while let line = iterator.next()?.trimmingCharacters(in: .whitespacesAndNewlines) {
+            switch line {
+            case _ where line.hasPrefix(ManifestTags.ExtInf.rawValue):
+                guard let duration = Double(line.filter("0123456789.".contains)) else {
+                    return
+                }
+                self.totalPlayListDuration += duration
+                self.constructedManifestArray.append(line)
+                if let nextLine = iterator.next()?.trimmingCharacters(in: .whitespacesAndNewlines), let fullURL = self.getFullURL(from: nextLine) {
+                    self.constructedManifestArray.append(fullURL.absoluteString)
+                }
+            default:
+                self.constructedManifestArray.append(line)
+                break;
+            }
+        }
+    }
+}
