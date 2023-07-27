84a66051d7829daa9da3b31207ea7fa8de3f0ea2
William Van Haevre <william.vanhaevre@gmail.com>
2023-07-27 15:12:56 +0200

    Add missing Ad metadata for conviva reporting

--- Code/Conviva/Source/Events/ConvivaReporters/AdEventConvivaReporter.swift
+++ Code/Conviva/Source/Events/ConvivaReporters/AdEventConvivaReporter.swift
@@ -106,14 +106,51 @@ public class AdEventConvivaReporter: AdEventProcessor, ConvivaAdPlaybackEventsRe
 extension Ad {
     /// A dictionary containing all the ad info that can be passed to `CISAdAnalytics`'s `setAdInfo(_ convivaInfo: [:])` function.
     var convivaInfo: [AnyHashable: Any] {
+        let googleImaAd = self as? GoogleImaAd
         var result: [AnyHashable: Any] = Utilities.playerInfo
-        result[CIS_SSDK_METADATA_ASSET_NAME] = id ?? Utilities.defaultStringValue
+        let assetName = nonEmpty(id) ?? Utilities.defaultStringValue
+        result[CIS_SSDK_METADATA_ASSET_NAME] = assetName
         result[CIS_SSDK_METADATA_STREAM_URL] = resourceURI ?? Utilities.defaultStringValue
-        if let linearAd = self as? LinearAd, let duration = linearAd.duration {
+        result["c3.ad.id"] = nonEmpty(id) ?? Utilities.defaultStringValue
+        result["c3.ad.creativeName"] = assetName
+        result["c3.ad.technology"] = AdTechnology.CLIENT_SIDE
+        result["c3.ad.isSlate"] = "false"
+        result["c3.ad.creativeId"] = nonEmpty(googleImaAd?.creativeId) ?? Utilities.defaultStringValue
+        result["c3.ad.system"] = nonEmpty(googleImaAd?.adSystem) ?? Utilities.defaultStringValue
+        result["c3.ad.firstAdId"] = nonEmpty(googleImaAd?.wrapperAdIds.first) ?? nonEmpty(id) ?? Utilities.defaultStringValue
+        result["c3.ad.firstCreativeId"] = nonEmpty(googleImaAd?.wrapperCreativeIds.first) ?? nonEmpty(googleImaAd?.creativeId) ?? Utilities.defaultStringValue
+        result["c3.ad.firstAdSystem"] = nonEmpty(googleImaAd?.wrapperAdSystems.first) ?? nonEmpty(googleImaAd?.adSystem) ?? Utilities.defaultStringValue
+        result["c3.ad.adStitcher"] = Utilities.defaultStringValue
+        result["c3.ad.position"] = self.calculateCurrentAdBreakPosition(adBreak: self.adBreak)
+        // linearAd specific
+        if let linearAd = self as? LinearAd,
+           let duration = linearAd.duration {
             result[CIS_SSDK_METADATA_IS_LIVE] = false
             result[CIS_SSDK_METADATA_DURATION] = NSNumber(value: Int(duration))
         }
+        
         return result
+    }
+    
+    private func calculateCurrentAdBreakPosition(adBreak: AdBreak?) -> String {
+        guard let adBr = adBreak else {
+            return "Mid-roll"
+        }
+        
+        if adBr.timeOffset == 0 {
+            return "Pre-roll"
+        } else if adBr.timeOffset < 0 {
+            return "Post-roll"
+        } else {
+            return "Mid-roll"
+        }
+    }
+    
+    private func nonEmpty(_ s: String?) -> String? {
+        guard let string = s else {
+            return nil
+        }
+        return string.count > 0 ? string : nil
     }
 }
 
