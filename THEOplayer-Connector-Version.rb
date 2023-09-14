90f0496695e912b0ba11a128cd170cea0f458efa
William Van Haevre <william.vanhaevre@gmail.com>
2023-09-14 14:15:02 +0200

    Bump version

--- THEOplayer-Connector-Version.rb
+++ THEOplayer-Connector-Version.rb
@@ -1,8 +1,8 @@
 def theoplayer_connector_major_minor_version
-  return '5.9'
+  return '5.11'
 end
 def theoplayer_connector_bug_version
-  return '1'
+  return '0'
 end
 def theoplayer_connector_version
   return theoplayer_connector_major_minor_version + '.' + theoplayer_connector_bug_version
