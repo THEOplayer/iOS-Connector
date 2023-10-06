949cd87e683c7376faa4b5de0de69901a93a0be7
Raffi <raffi@Raffis-MacBook-Pro.local>
2023-10-06 10:53:17 +0200

    bump to 6.1.1

--- THEOplayer-Connector-Version.rb
+++ THEOplayer-Connector-Version.rb
@@ -2,7 +2,7 @@ def theoplayer_connector_major_minor_version
   return '6.1'
 end
 def theoplayer_connector_bug_version
-  return '0'
+  return '1'
 end
 def theoplayer_connector_version
   return theoplayer_connector_major_minor_version + '.' + theoplayer_connector_bug_version
