864b4b6a039712c59237b574db5d5909a9d4798b
Raffi <raffi@Raffis-MacBook-Pro.local>
2023-10-19 15:11:37 +0200

    run update version only when argument passed

--- update_version_json.sh
+++ update_version_json.sh
@@ -7,12 +7,12 @@ done
 if [ -z "$version" ]
 then
    echo "Missing argument: 'version'. Usage: \$sh update_version_json.sh -v 1.0.0"
+else
+   v=$version
+   major_minor=${v%.*}
+   patch=$((${v##*.}))
+   echo $(cat version.json \
+      | jq --arg major_minor "$major_minor" '.major_minor=$major_minor' \
+      | jq --arg patch "$patch" '.patch=$patch') \
+      > version.json
 fi
-
-v=$version
-major_minor=${v%.*}
-patch=$((${v##*.}))
-echo $(cat version.json \
-	| jq --arg major_minor "$major_minor" '.major_minor=$major_minor' \
-	| jq --arg patch "$patch" '.patch=$patch') \
-	> version.json
