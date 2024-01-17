00f7fceaa10c8711563e0ed19d82ed0eaf1ed7ea
William Van Haevre <william.vanhaevre@gmail.com>
2024-01-17 16:35:11 +0100

    Distinguish between pre and post iOS 17 Notification Name for new accessLog entries

--- Code/Conviva/Source/Events/Observers/AppEventForwarder.swift
+++ Code/Conviva/Source/Events/Observers/AppEventForwarder.swift
@@ -11,10 +11,12 @@ import AVFoundation
 
 fileprivate let willEnterForeground = UIApplication.willEnterForegroundNotification
 fileprivate let didEnterBackground = UIApplication.didEnterBackgroundNotification
+fileprivate let oldAccessLogEntry = NSNotification.Name.AVPlayerItemNewAccessLogEntry
 fileprivate let newAccessLogEntry = AVPlayerItem.newAccessLogEntryNotification
 
 class AppEventForwarder {
     let center = NotificationCenter.default
+    let accessLogNotificationName: NSNotification.Name
     let foregroundObserver, backgroundObserver, accessLogObserver: Any
     let player: THEOplayer
     
@@ -33,8 +35,14 @@ class AppEventForwarder {
             queue: .none,
             using: eventProcessor.appDidEnterBackground
         )
+        
+        if #available(iOS 17.0, tvOS 17.0, *) {
+            accessLogNotificationName = Notification.Name("AVPlayerItemNewAccessLogEntry")
+        } else {
+            accessLogNotificationName = Notification.Name("AVPlayerItemNewAccessLogEntryNotification")
+        }
         accessLogObserver = center.addObserver( // TODO: implement this in THEOplayerSDK using an observer on the correct player item so we can remove src URL checks
-            forName: newAccessLogEntry,
+            forName: accessLogNotificationName,
             object: .none,
             queue: .none,
             using: { [unowned player] notification in
@@ -52,7 +60,7 @@ class AppEventForwarder {
     deinit {
         center.removeObserver(foregroundObserver, name: willEnterForeground, object: nil)
         center.removeObserver(backgroundObserver, name: didEnterBackground, object: nil)
-        center.removeObserver(accessLogObserver, name: newAccessLogEntry, object: nil)
+        center.removeObserver(accessLogObserver, name: self.accessLogNotificationName, object: nil)
     }
 }
 
