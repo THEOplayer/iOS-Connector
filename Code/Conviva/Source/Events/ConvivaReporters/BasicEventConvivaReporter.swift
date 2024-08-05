c2ac1e75d9e2ca3342bc7c37ecf23942fbaedf94
William Van Haevre <william.vanhaevre@gmail.com>
2024-08-05 10:48:25 +0200

    Only report one fatal error during a viewerSession

--- Code/Conviva/Source/Events/ConvivaReporters/BasicEventConvivaReporter.swift
+++ Code/Conviva/Source/Events/ConvivaReporters/BasicEventConvivaReporter.swift
@@ -59,7 +59,11 @@ class BasicEventConvivaReporter {
     }
     
     func error(event: ErrorEvent) {
-        self.videoAnalytics.reportPlaybackFailed(event.error, contentInfo: nil)
+        if self.currentSession.started {
+            self.videoAnalytics.reportPlaybackFailed(event.error, contentInfo: nil)
+            // the reportPlaybackFailed will close the session on the Conviva backend.
+            self.currentSession.started = false
+        }
     }
     
     func networkError(event: NetworkErrorEvent) {
