db7925779fdb076d0c80c5f8f260c9c2c3708210
Yousif <Yousif.khalid@dolby.com>
2025-03-03 12:27:04 +1100

    Fix pre-roll ad bitrate reporting to main content
    Sometimes with preroll ads we receive the
    ad break begin event after the access
    log entry for the ad is sent, which
    ends up reporting the ad bitrate to
    the main content. The workaround is
    to listen to the ad loaded event which
    seems to be guaranteed to come before
    we get the access log entry.

--- Code/Conviva/Source/Events/ConvivaReporters/AppEventConvivaReporter.swift
+++ Code/Conviva/Source/Events/ConvivaReporters/AppEventConvivaReporter.swift
@@ -4,13 +4,38 @@
 
 import ConvivaSDK
 import AVFoundation
+import THEOplayerSDK
 
-struct AppEventConvivaReporter: AppEventProcessor {
+class AppEventConvivaReporter: AppEventProcessor {
+
+    
     let analytics: CISAnalytics
     let videoAnalytics: CISVideoAnalytics
     let adAnalytics: CISAdAnalytics
     let storage: ConvivaConnectorStorage
-
+    
+    init(analytics: CISAnalytics, videoAnalytics: CISVideoAnalytics, adAnalytics: CISAdAnalytics, storage: ConvivaConnectorStorage) {
+        self.analytics = analytics
+        self.videoAnalytics = videoAnalytics
+        self.adAnalytics = adAnalytics
+        self.storage = storage
+    }
+    
+    // This is a workaround for THEOSD-14780
+    // Access log entries for preroll ads come in before the player.ads.playing is updated, which means
+    // bitrate information gets reported to the main content instead, which is incorrect.
+    // Ads get loaded always before access log entries come, which we can use to report
+    // the ad bitrate to the correct endpoint.
+    private var prerollAdPlaying: Bool = false
+    
+    func adDidLoad(event: THEOplayerSDK.AdLoadedEvent)  {
+        prerollAdPlaying = true
+    }
+    func adDidEnd(event: THEOplayerSDK.AdEndEvent)  {
+        prerollAdPlaying = false
+    }
+    
+    
     func appWillEnterForeground(notification: Notification) {
         self.analytics.reportAppForegrounded()
     }
@@ -20,8 +45,13 @@ struct AppEventConvivaReporter: AppEventProcessor {
     }
     
     func appGotNewAccessLogEntry(event: AVPlayerItemAccessLogEvent, isPlayingAd: Bool) {
-        let endpoint = isPlayingAd ? self.adAnalytics : self.videoAnalytics
-
+        // If the ad did load but we haven't started playing it,
+        // we will report the bitrate to the ad endpoint regardless.
+        // Previously, we were incorrectly reporting to the video endpoint
+        // when the ad loaded but was not playing.
+        let toSendToAdEndpoint = prerollAdPlaying || isPlayingAd
+        let endpoint = toSendToAdEndpoint ? self.adAnalytics : self.videoAnalytics
+        
         self.handleBitrateChange(bitrate: event.indicatedBitrate, endpoint: endpoint)
         
         if event.numberOfDroppedVideoFrames >= 0 {
@@ -30,7 +60,8 @@ struct AppEventConvivaReporter: AppEventProcessor {
     }
 
     func appGotBitrateChangeEvent(bitrate: Double, isPlayingAd: Bool) {
-        let endpoint = isPlayingAd ? self.adAnalytics : self.videoAnalytics
+        let toSendToAdEndpoint = prerollAdPlaying || isPlayingAd
+        let endpoint = toSendToAdEndpoint ? self.adAnalytics : self.videoAnalytics
         self.handleBitrateChange(bitrate: bitrate, endpoint: endpoint)
     }
 
