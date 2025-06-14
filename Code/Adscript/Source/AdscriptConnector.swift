7b9004cc3ac50cbe79f77a332c8c2288358f61b6
Wonne Joosen <wonne.joosen@theoplayer.com>
2025-06-14 23:51:09 +0200

    initial skeleton

---
+++ Code/Adscript/Source/AdscriptConnector.swift
@@ -0,0 +1,30 @@
+import THEOplayerSDK
+import AdScriptApiClient
+
+public struct AdscriptConfiguration {
+     let implementationId: String
+     let debug: Bool
+}
+
+public struct AdscriptConnector {
+    public let adapter: AdscriptAdapter
+    public let player: THEOplayer
+
+
+    public init(configuration: AdscriptConfiguration, player: THEOplayer, metadata: AdScriptDataObject) {
+        self.player = player
+        self.adapter = AdscriptAdapter(configuration: configuration, player: player, metadata: metadata)
+    }
+    
+    public func sessionStart() {
+        self.adapter.sessionStart()
+    }
+    
+    public func update(metadata: AdScriptDataObject) {
+        self.adapter.update(metadata: metadata)
+    }
+    
+    public func updateUser(i12n: AdScriptI12n) {
+        self.adapter.updateUser(i12n: i12n)
+    }
+}
