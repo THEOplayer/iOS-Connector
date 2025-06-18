b65559128918f001887386f737e37946532801bf
Wonne Joosen <wonne.joosen@theoplayer.com>
2025-06-18 09:47:37 +0200

    initial player state reporting

--- Code/Adscript/Source/integration/AdscriptAdapter.swift
+++ Code/Adscript/Source/integration/AdscriptAdapter.swift
@@ -14,6 +14,7 @@ public struct AdscriptAdapter {
         
         self.adscriptCollector = AdScriptCollector(implementationId: configuration.implementationId, isDebug: configuration.debug)
         
+        reportPlayerState()
         addEventListeners()
     }
     
@@ -26,6 +27,40 @@ public struct AdscriptAdapter {
     }
     
     public func updateUser(i12n: AdScriptI12n) {
+        // TODO
+    }
+    
+    private func reportPlayerState() {
+        reportFullscreen(isFullscreen: player.presentationMode == PresentationMode.fullscreen)
+        reportDimensions(width: player.videoWidth, height: player.videoHeight)
+        reportPlaybackSpeed(playbackRate: player.playbackRate)
+        reportVolumeAndMuted(isMuted: player.muted, volume: player.volume)
+        reportTriggeredByUser(autoplayEnabled: player.autoplay)
+        reportVisibility()
+    }
+    
+    private func reportFullscreen(isFullscreen: Bool) {
+        _ = self.adscriptCollector.playerState.set(key: AdScriptPlayerStateKey.fullscreen, value: isFullscreen ? 1 : 0)
+    }
+    
+    private func reportDimensions(width: Int, height: Int) {
+        _ = self.adscriptCollector.playerState.set(key: AdScriptPlayerStateKey.width, value: width)
+        _ = self.adscriptCollector.playerState.set(key: AdScriptPlayerStateKey.height, value: height)
+    }
+    
+    private func reportPlaybackSpeed(playbackRate: Double) {
+        _ = self.adscriptCollector.playerState.set(key: AdScriptPlayerStateKey.normalSpeed, value: playbackRate == 1 ? 1 : 0)
+    }
+    
+    private func reportVolumeAndMuted(isMuted: Bool, volume: Float) {
+        _ = self.adscriptCollector.playerState.set(key: AdScriptPlayerStateKey.muted, value: (isMuted || volume == 0) ? 1 : 0)
+        _ = self.adscriptCollector.playerState.set(key: AdScriptPlayerStateKey.volume, value: Int(volume))
+    }
+    private func reportTriggeredByUser(autoplayEnabled: Bool) {
+        _ = self.adscriptCollector.playerState.set(key: AdScriptPlayerStateKey.triggeredByUser, value: autoplayEnabled ? 1 : 0)
+    }
+    
+    private func reportVisibility() {
         // TODO
     }
     
