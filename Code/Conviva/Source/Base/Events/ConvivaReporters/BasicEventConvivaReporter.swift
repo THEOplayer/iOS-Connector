6e0adff2476ae9d31dc3f2efb2752ea8032a059f
William Van Haevre <william.vanhaevre@gmail.com>
2025-10-01 14:42:30 +0200

    Fix destroy flow

--- Code/Conviva/Source/Base/Events/ConvivaReporters/BasicEventConvivaReporter.swift
+++ Code/Conviva/Source/Base/Events/ConvivaReporters/BasicEventConvivaReporter.swift
@@ -174,7 +174,11 @@ class BasicEventConvivaReporter {
         }
     }
     
-    func destroy(event: DestroyEvent) {
+    func onDestroy(event: DestroyEvent) {
+        self.destroy()
+    }
+    
+    func destroy() {
         self.reportEndedIfPlayed()
     }
     
