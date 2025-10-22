584f8350b37c351c45bda232e18cf112112df8d7
William Van Haevre <william.vanhaevre@gmail.com>
2025-10-22 15:12:14 +0200

    Drop unused appGotNewAccessLogEntry method

--- Code/Conviva/Source/Base/Events/ConvivaHandlers/AppHandler.swift
+++ Code/Conviva/Source/Base/Events/ConvivaHandlers/AppHandler.swift
@@ -46,48 +46,6 @@ class AppHandler {
         self.endpoints?.analytics.reportAppBackgrounded()
     }
     
-    func appGotNewAccessLogEntry(event: AVPlayerItemAccessLogEvent, item: AVPlayerItem, isPlayingAd: Bool) {
-        log("handling appGotNewAccessLogEntry")
-        // This indicates that the current access log we got belongs
-        // to an ad that is not playing, we will receive the same
-        // access log bitrate shortly after when we start playing, so
-        // we can ignore this bitrate.
-        if self.adLoaded, !isPlayingAd {
-            return
-        }
-        
-        // if we get the same number of dropped frames for the same item in a row, we don't need to report that.
-        // I see this happens when we get an access log towards the end of an ad.
-        if self.lastPlayerItem == item,
-           event.numberOfDroppedVideoFrames == event.numberOfDroppedVideoFrames
-        {
-            return
-        }
-        
-        if let endpoint: CISStreamAnalyticsProtocol = isPlayingAd ? self.endpoints?.adAnalytics : self.endpoints?.videoAnalytics {
-            if event.numberOfDroppedVideoFrames >= 0 {
-                let droppedFrames = NSNumber(value: event.numberOfDroppedVideoFrames)
-                log("endpoint.reportPlaybackMetric [CIS_SSDK_PLAYBACK_METRIC_DROPPED_FRAMES_TOTAL : \(droppedFrames)]")
-                endpoint.reportPlaybackMetric(CIS_SSDK_PLAYBACK_METRIC_DROPPED_FRAMES_TOTAL, value: droppedFrames)
-            }
-            if event.indicatedBitrate >= 0 {
-                let bitrateValue = NSNumber(value: event.indicatedBitrate / 1000)
-                log("endpoint.reportPlaybackMetric [CIS_SSDK_PLAYBACK_METRIC_BITRATE : \(bitrateValue)]")
-                endpoint.reportPlaybackMetric(CIS_SSDK_PLAYBACK_METRIC_BITRATE, value: bitrateValue)
-                self.storage?.storeMetric(key: CIS_SSDK_PLAYBACK_METRIC_BITRATE, value: bitrateValue)
-            }
-            if event.indicatedAverageBitrate >= 0 {
-                let avgBitrateValue = NSNumber(value: event.indicatedAverageBitrate / 1000)
-                log("endpoint.reportPlaybackMetric [CIS_SSDK_PLAYBACK_METRIC_AVERAGE_BITRATE : \(avgBitrateValue)]")
-                endpoint.reportPlaybackMetric(CIS_SSDK_PLAYBACK_METRIC_AVERAGE_BITRATE, value: avgBitrateValue)
-                self.storage?.storeMetric(key: CIS_SSDK_PLAYBACK_METRIC_AVERAGE_BITRATE, value: avgBitrateValue)
-            }
-        }
-        
-        self.lastPlayerItem = item
-        self.lastAccessLogEvent = event
-    }
-    
     private func reset() {
         log("reset")
         self.adLoaded = false
